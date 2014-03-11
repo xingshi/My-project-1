9c2437d3be47ab0ac37000c66da2d54504f7ef0c
Shi Xing <xingshi0421@gmail.com>
2014-03-11 21:48:43 +1100

    small testing on story page
    Use Jquery toggle to toggle stories

--- all-story.php
+++ all-story.php
@@ -12,11 +12,11 @@
         <li>
           <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
           <div class="timeline-panel">
-            <div class="timeline-heading">
+            <div class="timeline-heading" id="toggle-key">
               <h4 class="timeline-title">Mussum ipsum cacilds</h4>
               <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
             </div>
-            <div class="timeline-body">
+            <div class="timeline-body" id="toggle-content1">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
             </div>
           </div>
@@ -27,7 +27,7 @@
             <div class="timeline-heading">
               <h4 class="timeline-title">Mussum ipsum cacilds</h4>
             </div>
-            <div class="timeline-body">
+            <div class="timeline-body" id="toggle-content2">
               <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
               <p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
             </div>
@@ -101,4 +101,11 @@
         </li>
     </ul>
 </div>
+<script>
+  jQuery( document ).ready(function($) {
+    $( "#toggle-key" ).click(function() {
+      $( "div[id*='toggle-content']" ).slideToggle( "slow" );
+    });
+  });
+</script>
 <?php get_footer(); ?>
